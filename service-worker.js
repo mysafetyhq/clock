0f6fcc6247885b203d74093d8475f1977d0fbcda
mysafetyhq <164397962+mysafetyhq@users.noreply.github.com>
2024-10-30 09:14:08 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                'index.html',
+                'site.webmanifest',
+                '/web-app-manifest-192x192.png',
+                '/web-app-manifest-512x512.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
